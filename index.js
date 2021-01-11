86043f9a95246dc52073fe0f7d9a53992125dfd3
Permafun <49506518+Permafun@users.noreply.github.com>
2021-01-11 16:26:51 -0600

    Adding Profile and converting index.js for Heroku

--- index.js
+++ index.js
@@ -45,7 +45,11 @@ mongoose
 		{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
 	)
 	.then(() => {
-		app.listen(5000);
+		let port = process.env.PORT;
+		if (port == null || port == '') {
+			port = 5000;
+		}
+		app.listen(port);
 	})
 	.catch((err) => {
 		console.log(err);
